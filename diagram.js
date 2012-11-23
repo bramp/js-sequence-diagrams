406969e7ce1728b433b7bfa8d0edb8ae5a5bb381
Andrew Brampton <me@bramp.net>
2012-11-23 15:10:03 -0500

    JSHint fixes.

--- diagram.js
+++ diagram.js
@@ -1,5 +1,6 @@
 (function () {
 	"use strict";
+	/*global grammar */
 
 	function Diagram() {
 		this.title   = undefined;
@@ -8,27 +9,28 @@
 	}
 
 	Diagram.prototype.getActor = function(name) {
+		var i;
 		var actors = this.actors;
-		for (var i in actors) {
+		for (i in actors) {
 			if (actors[i].name == name)
 				return actors[i];
 		}
-		var i = actors.push( new Diagram.Actor(name, actors.length) );
+		i = actors.push( new Diagram.Actor(name, actors.length) );
 		return actors[ i - 1 ];
-	}
+	};
 
 	Diagram.prototype.setTitle = function(title) {
 		this.title = title;
-	}
+	};
 
 	Diagram.prototype.addSignal = function(signal) {
 		this.signals.push( signal );
-	}
+	};
 
 	Diagram.Actor = function(name, index) {
 		this.name = name;
 		this.index = index;
-	}
+	};
 
 	Diagram.Signal = function(actorA, signaltype, actorB, message) {
 		this.type       = "Signal";
@@ -37,14 +39,14 @@
 		this.linetype   = signaltype;
 		this.arrowtype  = signaltype >> 2;
 		this.message    = message;
-	}
+	};
 
 	Diagram.Note = function(actor, placement, message) {
 		this.type      = "Note";
 		this.actor     = actor;
 		this.placement = placement;
 		this.message   = message;
-	}
+	};
 
 	Diagram.LINETYPE = {
 		SOLID  : 0,
@@ -69,27 +71,27 @@
 	 * jison doesn't have a good exception, so we make one
 	 */
 	function ParseError(message, hash) {
-	    this.name = "ParseError";
-	    this.message = (message || "");
-	    this.text  = hash.text;
-	    this.token = hash.token;
-	    this.line  = hash.line;
-	    this.loc   = hash.loc;
-	    this.expected = hash.expected;
+		this.name = "ParseError";
+		this.message = (message || "");
+		this.text  = hash.text;
+		this.token = hash.token;
+		this.line  = hash.line;
+		this.loc   = hash.loc;
+		this.expected = hash.expected;
 	}
 	ParseError.prototype = new Error();
 	Diagram.ParseError = ParseError;
 
 	grammar.parseError = function(message, hash) {
-		throw new ParseError(message, hash)
-	}
+		throw new ParseError(message, hash);
+	};
 
 	Diagram.parse = function(input) {	
 		//var parser = require("grammar").parser;
 		grammar.yy = new Diagram();
 
 		return grammar.parse(input);
-	}
+	};
 
 	// Expose this class externally
 	this.Diagram = Diagram;
