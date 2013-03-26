ae43460366b8c80ab4b9f5e148d11c2125d5603b
Andrew Brampton <me@bramp.net>
2013-03-25 22:52:32 -0400

    Changed the jquery plugin to gracefully fail if jquery is missing, as it's not really required.

--- src/jquery-plugin.js
+++ src/jquery-plugin.js
@@ -1,10 +1,12 @@
-(function( $ ) {
-	$.fn.sequenceDiagram = function( options ) {
-		return this.each(function() {
-			var $this = $(this);
-			var diagram = Diagram.parse($this.text());
-			$this.html('');
-			diagram.drawSVG(this, options);
-		});
-	};
-})( jQuery );
+if (typeof jQuery != 'undefined') {
+	(function( $ ) {
+		$.fn.sequenceDiagram = function( options ) {
+			return this.each(function() {
+				var $this = $(this);
+				var diagram = Diagram.parse($this.text());
+				$this.html('');
+				diagram.drawSVG(this, options);
+			});
+		};
+	})( jQuery );
+}
