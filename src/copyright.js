861a19644cbc2aa3da4c3cb2ea7893297e6fbdd0
Andrew Brampton <me@bramp.net>
2013-03-31 15:07:46 -0400

    Release version 1.0.2.

--- src/copyright.js
+++ src/copyright.js
@@ -1,4 +1,4 @@
-/** js sequence diagrams 1.0.1
+/** js sequence diagrams 1.0.2
  *  http://bramp.github.com/js-sequence-diagrams/
  *  (c) 2012-2013 Andrew Brampton (bramp.net)
  *  @license Simplified BSD license.
