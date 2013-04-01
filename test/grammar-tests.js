e7b9e73e1419a9320a8fc9c7b7d4cc4fbdcb2de6
Andrew Brampton <me@bramp.net>
2013-03-31 20:17:06 -0400

    Updated the unit tests to refactor them to be simplier, and added new notes tests.

--- test/grammar-tests.js
+++ test/grammar-tests.js
@@ -1,4 +1,4 @@
-function assertSingleArrow(d) {
+function assertSingleArrow(d, arrowtype, linetype) {
 
 	equal(d.actors.length, 2, "Correct actors count");
 
@@ -7,39 +7,71 @@ function assertSingleArrow(d) {
 	equal(a.name, "A", "Actors A");
 	equal(b.name, "B", "Actors B");
 
-	equal(d.signals[0].message, "Title", "Signal message");
-
 	equal(d.signals.length, 1, "Correct signals count");
+	equal(d.signals[0].type, "Signal", "Correct signal type");
 	equal(d.signals[0].actorA, a, "Actors A");
 	equal(d.signals[0].actorB, b, "Actors B");
+
+	equal(d.signals[0].message, "Title", "Signal message");
+
+	equal(d.signals[0].arrowtype, arrowtype, "Arrowhead type");
+	equal(d.signals[0].linetype, linetype, "Line type");
 }
+
+function assertSingleNote(d, placement, actors) {
+
+	equal(d.signals.length, 1, "Correct notes count");
+	
+	var note = d.signals[0];
+	equal(note.type, "Note", "Correct signal type");
+	equal(note.placement, placement, "Correct signal placement");
+	equal(note.message, "Title", "Correct signal message");
+
+	if (_.isArray(actors)) {
+		equal(_.isArray(d.actors), true, "Correct actors array");
+		equal(d.actors.length, actors.length, "Correct actors count");
+
+		equal(note.actor.length, actors.length, "Correct note actors");
+		for (var i = 0; i < actors.length; i++) {
+			equal(d.actors[i].name, actors[i], "Correct actor");
+			equal(note.actor[i].name, actors[i], "Correct note actor");
+		}
+		
+	} else {
+		equal(d.actors.length, 1, "Correct actors count");
+		equal(note.actor.name, actors, "Correct note actor");
+	}
+}
+
+function assertEmptyDocument(d) {
+	equal(d.title, undefined, "No title");
+	equal(d.actors.length,  0, "Zero actors");
+	equal(d.signals.length, 0, "Zero signals");
+}
+
+
+var LINETYPE = Diagram.LINETYPE;
+var ARROWTYPE = Diagram.ARROWTYPE;
+var PLACEMENT = Diagram.PLACEMENT;
 
 test( "Solid Arrow", function() {
 	var d = Diagram.parse("A->B: Title");
-	assertSingleArrow(d);
-	equal(d.signals[0].arrowtype, 0, "Arrowhead type");
-	equal(d.signals[0].linetype, 0, "Line type");
+	assertSingleArrow(d, ARROWTYPE.FILLED, LINETYPE.SOLID);
 });
 
 test( "Dashed Arrow", function() {
 	var d = Diagram.parse("A-->B: Title");
-	assertSingleArrow(d);
-	equal(d.signals[0].arrowtype, 0, "Arrowhead type");
-	equal(d.signals[0].linetype, 1, "Line type");
+	assertSingleArrow(d, ARROWTYPE.FILLED, LINETYPE.DOTTED);
 });
 
 test( "Solid Open Arrow", function() {
 	var d = Diagram.parse("A->>B: Title");
-	assertSingleArrow(d);
-	equal(d.signals[0].arrowtype, 1, "Arrowhead type");
-	equal(d.signals[0].linetype, 0, "Line type");
+	assertSingleArrow(d, ARROWTYPE.OPEN, LINETYPE.SOLID);
 });
 
 test( "Dashed Open Arrow", function() {
 	var d = Diagram.parse("A-->>B: Title");
-	assertSingleArrow(d);
-	equal(d.signals[0].arrowtype, 1, "Arrowhead type");
-	equal(d.signals[0].linetype, 1, "Line type");
+	assertSingleArrow(d, ARROWTYPE.OPEN, LINETYPE.DOTTED);
 });
 
 test( "Titles", function() {
@@ -48,12 +80,6 @@ test( "Titles", function() {
 
 	equal(Diagram.parse("Title: 中国").title, "中国", "Unicode Title");
 });
-
-function assertEmptyDocument(d) {
-	equal(d.title, undefined, "No title");
-	equal(d.actors.length,  0, "Zero actors");
-	equal(d.signals.length, 0, "Zero signals");
-}
 
 test( "Empty documents", function() {
 	assertEmptyDocument(Diagram.parse(""));
@@ -65,8 +91,19 @@ test( "Comments", function() {
 	assertEmptyDocument(Diagram.parse("# comment"));
 	assertEmptyDocument(Diagram.parse(" # comment"));
 	assertEmptyDocument(Diagram.parse("# A->B: Title"));
-	assertSingleArrow(Diagram.parse("A->B: Title # comment"));
+	assertSingleArrow(Diagram.parse("A->B: Title # comment"), 0, 0);
 	//"title Title # comment"
 	//"participant A # comment"
 	//"note left of A: blah # comment"
 });
+
+test( "Notes", function() {
+	assertSingleNote(Diagram.parse("Note left of A: Title"), PLACEMENT.LEFTOF, 'A');
+	assertSingleNote(Diagram.parse("Note right of A: Title"), PLACEMENT.RIGHTOF, 'A');
+	assertSingleNote(Diagram.parse("Note over A: Title"), PLACEMENT.OVER, 'A');
+	assertSingleNote(Diagram.parse("Note over A,B: Title"), PLACEMENT.OVER, ['A', 'B']);
+	//"title Title # comment"
+	//"participant A # comment"
+	//"note left of A: blah # comment"
+});
+
