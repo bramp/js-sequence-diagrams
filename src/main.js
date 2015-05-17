cc908a100e2b3d3df3ef471562c711fd9e0b7203
Andrew Brampton <github@bramp.net>
2015-05-16 19:40:17 -0700

    Released version 1.0.6 - To make bower work

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-/** js sequence diagrams 1.0.5
+/** js sequence diagrams 1.0.6
  *  http://bramp.github.io/js-sequence-diagrams/
  *  (c) 2012-2015 Andrew Brampton (bramp.net)
  *  @license Simplified BSD license.
