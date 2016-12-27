88b16473e5b144a8bcd758edeab29cb5acb73704
Andrew Brampton <bramp@google.com>
2016-12-26 23:02:10 -0800

    Released version 2.0.0

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-/** js sequence diagrams 1.0.6
+/** js sequence diagrams 2.0
  *  https://bramp.github.io/js-sequence-diagrams/
  *  (c) 2012-2017 Andrew Brampton (bramp.net)
  *  @license Simplified BSD license.
